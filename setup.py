7bf37a26bee9aa9ed70f78f85d3e4be94d12cc7d
local <local@yoav-ubuntu-vm.il.nds.com>
2014-04-06 13:00:09 +0300

    initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='JyDev',
+      version='0.1',
+      description='Jython Project Maker/Manager with pydev support',
+      author='Yoav Glazner',
+      author_email='yoavglazner@gmail.com',
+      url='some url to git?',
+      packages=['jy_dev'],
+     )
