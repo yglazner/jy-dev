c4e0414249b1f23080a5252521a45f3e007f0c7b
Yoav Glazner <yoavglazner@gmail.com>
2014-04-06 17:52:01 +0300

    adding __main__ to manifest

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from distutils.core import setup
 
 setup(name='JyDev',
-      version='0.3',
+      version='0.31',
       description='Jython Project Maker/Manager with pydev support',
       author='Yoav Glazner',
       author_email='yoavglazner@gmail.com',
       url='https://github.com/yglazner/jy-dev',
-      packages=['jy_dev', 'jy_dev.__main__.py'],
+      packages=['jy_dev'],
      )
